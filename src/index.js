685996ca83fe45e819e8fa6f040ee9e4e1d1ac46
xinsanitiix <xinsanitiix@gmail.com>
2025-12-21 18:19:41 -0500

    targeted 4 cities with js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let date1Element = document.querySelector(".date-1");
+date1Element.innerHTML = moment().format("MMMM Do YYYY");
+
+let time1Element = document.querySelector(".time-1");
+time1Element.innerHTML = "6:14";
+
+let date2Element = document.querySelector(".date-2");
+date2Element.innerHTML = "Dec 3rd";
+
+let time2Element = document.querySelector(".time-2");
+time2Element.innerHTML = "6:14";
+
+let date3Element = document.querySelector(".date-3");
+date3Element.innerHTML = "Aug 16 th";
+
+let time3Element = document.querySelector(".time-3");
+time3Element.innerHTML = "6:14";
+
+let date4Element = document.querySelector(".date-4");
+date4Element.innerHTML = "May 22nd";
+
+let time4Element = document.querySelector(".time-4");
+time4Element.innerHTML = "6:14";
