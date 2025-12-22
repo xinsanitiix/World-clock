577019b6c7a282b36533fd438858845471d23b30
xinsanitiix <xinsanitiix@gmail.com>
2025-12-22 12:39:22 -0500

    js to update time in 4 cities

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
-let date1Element = document.querySelector(".date-1");
-date1Element.innerHTML = moment().format("MMMM Do YYYY");
+function updateTime() {
+  let date1Element = document.querySelector(".date-1");
+  date1Element.innerHTML = moment().tz("Europe/Madrid").format("MMMM Do YYYY");
 
-let time1Element = document.querySelector(".time-1");
-time1Element.innerHTML = "6:14";
+  let time1Element = document.querySelector(".time-1");
+  time1Element.innerHTML =
+    moment().tz("Europe/Madrid").format("h:mm:ss") +
+    " " +
+    moment().tz("Europe/Madrid").format("a");
+  let date2Element = document.querySelector(".date-2");
+  date2Element.innerHTML = moment()
+    .tz("America/New_York")
+    .format("MMMM Do YYYY");
 
-let date2Element = document.querySelector(".date-2");
-date2Element.innerHTML = "Dec 3rd";
+  let time2Element = document.querySelector(".time-2");
+  time2Element.innerHTML =
+    moment().tz("America/New_York").format("h:mm:ss") +
+    " " +
+    moment().tz("America/New_York").format("a");
 
-let time2Element = document.querySelector(".time-2");
-time2Element.innerHTML = "6:14";
+  let date3Element = document.querySelector(".date-3");
+  date3Element.innerHTML = moment().tz("Asia/Seoul").format("MMMM Do YYYY");
 
-let date3Element = document.querySelector(".date-3");
-date3Element.innerHTML = "Aug 16 th";
+  let time3Element = document.querySelector(".time-3");
+  time3Element.innerHTML =
+    moment().tz("Asia/Seoul").format("h:mm:ss") +
+    " " +
+    moment().tz("Asia/Seoul").format("a");
 
-let time3Element = document.querySelector(".time-3");
-time3Element.innerHTML = "6:14";
+  let date4Element = document.querySelector(".date-4");
+  date4Element.innerHTML = moment().tz("Africa/Cairo").format("MMMM Do YYYY");
 
-let date4Element = document.querySelector(".date-4");
-date4Element.innerHTML = "May 22nd";
+  let time4Element = document.querySelector(".time-4");
+  time4Element.innerHTML =
+    moment().tz("Africa/Cairo").format("h:mm:ss") +
+    " " +
+    moment().tz("Africa/Cairo").format("a");
+}
 
-let time4Element = document.querySelector(".time-4");
-time4Element.innerHTML = "6:14";
+updateTime();
+setInterval(updateTime, 1000);
